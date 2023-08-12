8d978ba6c2950148226bc0d710e0d329ebbed08e
algomonk016 <stshandilya0820@gmail.com>
2023-08-12 21:49:33 +0530

    feat: added api caching

--- src/screens/Home/services/pokemon.ts
+++ src/screens/Home/services/pokemon.ts
@@ -1,12 +1,17 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import type {TPokemon} from './types';
 
+const CACHE_TIME = 15; // minutes
+
 export const pokemonApi = createApi({
   reducerPath: 'pokemonApi',
-  baseQuery: fetchBaseQuery({baseUrl: 'https://pokeapi.co/api/v2'}),
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://pokeapi.co/api/v2',
+  }),
   endpoints: builder => ({
     getPokemonByName: builder.query<TPokemon, string>({
       query: name => `pokemon/${name}`,
+      keepUnusedDataFor: CACHE_TIME * 60,
     }),
   }),
 });
