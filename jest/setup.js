e69944a007e28c6dc39d99bb2fcbf69917a21259
algomonk016 <stshandilya0820@gmail.com>
2023-08-19 06:08:00 +0530

    test(mock): added mock for async-storage

--- jest/setup.js
+++ jest/setup.js
@@ -14,5 +14,10 @@ jest.mock('react-native-reanimated', () => {
   return Reanimated;
 });
 
+// mock async storage
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock'),
+);
+
 // Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
